cfab3372d876843e4342b1d0dbced1d38fc5f98f
Peter Sharp <peter.victor.sharp@gmail.com>
2018-08-30 22:31:52 +1200

    copied in demo code, added start button and output

--- client.js
+++ client.js
@@ -1,4 +1,60 @@
-const $magnet = window.magnet_input
+const $magnetInput = window.magnet_input
+const $magnet = window.magnet
+const $messages = window.messages
 const $messageForm = window.message_form
+const $startButton = window.start_button
+var localConnection;
+var remoteConnection;
+var sendChannel;
+var receiveChannel;
+var pcConstraint;
+var dataConstraint;
 
-debugger
+
+
+function createConnection() {
+  $messageForm.placeholder = '';
+  var servers = null;
+  pcConstraint = null;
+  dataConstraint = null;
+  trace('Using SCTP based data channels');
+  // For SCTP, reliable and ordered delivery is true by default.
+  // Add localConnection to global scope to make it visible
+  // from the browser console.
+  window.localConnection = localConnection =
+      new RTCPeerConnection(servers, pcConstraint);
+  trace('Created local peer connection object localConnection');
+
+  sendChannel = localConnection.createDataChannel('sendDataChannel',
+      dataConstraint);
+  trace('Created send data channel');
+
+  localConnection.onicecandidate = iceCallback1;
+  sendChannel.onopen = onSendChannelStateChange;
+  sendChannel.onclose = onSendChannelStateChange;
+
+  // Add remoteConnection to global scope to make it visible
+  // from the browser console.
+  window.remoteConnection = remoteConnection =
+      new RTCPeerConnection(servers, pcConstraint);
+  trace('Created remote peer connection object remoteConnection');
+
+  remoteConnection.onicecandidate = iceCallback2;
+  remoteConnection.ondatachannel = receiveChannelCallback;
+
+  localConnection.createOffer().then(
+    gotDescription1,
+    onCreateSessionDescriptionError
+  );
+  startButton.disabled = true;
+  closeButton.disabled = false;
+}
+
+$messageForm.addEventListener('submit', sendData);
+
+function sendData(ev) {
+  ev.preventDefault()
+  var data = dataChannelSend.value;
+  sendChannel.send(data);
+  trace('Sent Data: ' + data);
+}
